886ad50c011faa4dd2e94899e6b79256f0a85f2b
fabiolorenzi <fabiolorenzi13975@gmail.com>
2022-05-20 15:22:53 +0100

    added array

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function myview() {
-    this.username = ko.observable("fabio");
+    this.username = ko.observable("bla");
     this.count = ko.observable(0);
     this.decrease = function() {
         let tempCount = this.count();
@@ -9,6 +9,11 @@ function myview() {
         let tempCount = this.count();
         this.count(tempCount + 1);
     };
+
+    this.inventory = ko.observableArray([
+        {name: "Jonny", age: 23},
+        {name: "Jack", age: 26}
+    ]);
 };
 
 ko.applyBindings(new myview());
